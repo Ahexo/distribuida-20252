1967be2dd0096cf27389ff8259e6412de2ff8cdd
Ahexo <ahexo@ciencias.unam.mx>
2025-03-25 11:16:08 -0600

    ADD: Nueva clase nodo con soporte nativo para simpy

---
+++ practica3/practica03.py
@@ -0,0 +1,82 @@
+import simpy
+import argparse
+import random
+
+parser = argparse.ArgumentParser(
+    prog="practica3",
+    description="Un programa para simular la red de un sistema distribuido y hallar el diámetro de su gráfica subyacente.",
+    epilog="Implementado por Axel Rodríguez y José David Aguilar para el curso de Computación Distribuida 7106 2025-2.",
+)
+
+parser.add_argument(
+    "nodos",
+    type=int,
+    default=8,
+    help="Número de nodos que tendrá el sistema distribuido",
+)
+
+
+class Nodo:
+    def __init__(self, pid: int, env):
+        self.pid = pid
+        self.vecinos = set()
+        self.env = env
+        self.action = env.process(self.iterar())
+        self.dummy = 0
+
+    def __repr__(self):
+        return f"<{str(self.pid)}>"
+
+    def iterar(self):
+        self.dummy += 1
+        print(f"{self} está iterando en {self.dummy}")
+        yield self.env.timeout(1)
+        self.iterar()
+
+
+"""
+    Genera el número de nodos (que son objetos de la clase nodo) que se
+    especifica como argumento y los une en una gŕafica.
+
+    En un principio, todos los nodos estarán unidos linealmente en un solo
+    camino. Posteriormente, para todo par de vértices en la gráfica, se
+    agregará un vértice que los una con probabilidad del 50%.
+
+    Parameters
+    ----------
+    grado: int
+        Número de nodos/vertices de la gráfica.
+
+    Returns
+    -------
+    dict:
+        Diccionario de la forma {pid: nodo}
+"""
+
+
+def construir_grafica(grado: int, env):
+    # Generamos el número de nodos especificado
+    nodos = {i: Nodo(i, env) for i in range(1, grado + 1)}
+    # Los unimos linealmente
+    for i in range(1, grado):
+        nodos[i].vecinos.add(nodos[i + 1])
+        nodos[i + 1].vecinos.add(nodos[i])
+    # Añadimos aristas con probabilidad del 50% para todo par de nodos.
+    for i in range(1, grado + 1):
+        for j in range(i + 1, grado + 1):
+            if random.random() < 0.5:
+                nodos[i].vecinos.add(nodos[j])
+                nodos[j].vecinos.add(nodos[i])
+    return nodos
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    env = simpy.Environment()
+    grafica = construir_grafica(args.nodos, env)
+    print(
+        f"Se ha generado una red de nodos de grado {args.nodos} con las siguientes adyacencias:"
+    )
+    for nodo in grafica:
+        print(f"{grafica[nodo]}: {grafica[nodo].vecinos}")
+    env.run()
