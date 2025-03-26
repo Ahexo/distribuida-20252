c55ddfaeae4d57c6c4ddab9e445603cbc52aa590
Ahexo <ahexo@ciencias.unam.mx>
2025-03-25 19:15:45 -0600

    ADD: Algoritmo DFS en una clase diferente

--- practica3/practica03.py
+++ practica3/practica03.py
@@ -2,6 +2,7 @@ import simpy
 import argparse
 import random
 from collections import deque
+from DFS import DFS
 
 parser = argparse.ArgumentParser(
     prog="practica3",
@@ -17,7 +18,7 @@ parser.add_argument(
 )
 
 
-class Nodo:
+class Nodo(DFS):
     def __init__(self, pid: int, env):
         self.pid = pid
         self.env = env
@@ -25,9 +26,10 @@ class Nodo:
         self.action = env.process(self.iterar())
         self.continuar = True
         self.cola_mensajes = deque()
+        DFS.__init__(self)
 
     def __repr__(self):
-        return f"<{str(self.pid)}>"
+        return f"<{self.pid}>"
 
     """
     1. Leer los mensajes que hayan llegado en la ronda pasada, solo uno
@@ -37,16 +39,22 @@ class Nodo:
     3. Enviar mensajes si así se ha resuelto en el paso anterior, solo
     uno por vecino.
     """
+
     def iterar(self):
         while self.continuar:
             print(f"[Ronda {self.env.now}] {self} está iterando.")
 
             # Leer los mensajes (uno por cada vecino)
             mensajes_por_vecino = {}
-            while self.cola_mensajes:
-                metodo, args, remitente, tiempo = self.cola_mensajes.popleft()
-                if remitente not in mensajes_por_vecino:
+            for i in range(0, len(self.cola_mensajes)):
+                mensaje = self.cola_mensajes.popleft()
+                metodo, args, remitente, tiempo = mensaje
+                if (remitente not in mensajes_por_vecino) and (
+                    tiempo is not self.env.now
+                ):
                     mensajes_por_vecino[remitente] = (metodo, args, tiempo)
+                else:
+                    self.cola_mensajes.append(mensaje)
 
             # Procesar los mensajes recibidos
             for remitente, (metodo, args, tiempo) in mensajes_por_vecino.items():
@@ -56,7 +64,9 @@ class Nodo:
 
     """
     Agrega un mensaje a la cola de mensajes del nodo.
+    Los mensajes se deberán de mandar en una tupla o un diccionario.
     """
+
     def msg(self, metodo: str, args, remitente):
         mensaje = (metodo, args, remitente, self.env.now)
         self.cola_mensajes.append(mensaje)
@@ -64,24 +74,36 @@ class Nodo:
     """
     Procesa un mensaje llamando al método correspondiente si existe.
     """
+
     def procesar_mensaje(self, metodo, args, remitente, tiempo):
         if hasattr(self, metodo):  # Verifica si el nodo tiene el método
             metodo_a_llamar = getattr(self, metodo)
-            if isinstance(args, dict):  # Si los argumentos son un diccionario, usar **args
-                args['remitente'] = remitente
+            if isinstance(args, dict):
+                # Si los argumentos son un diccionario, usar **args
+                args["remitente"] = remitente
                 metodo_a_llamar(**args)
-            elif isinstance(args, tuple):  # Si son una tupla, pasarlos como argumentos
+            elif isinstance(args, tuple):
+                # Si son una tupla, pasarlos como argumentos
                 metodo_a_llamar(*args, remitente)
-            else:  # Si es un único argumento, pasarlo tal cual
+            else:
+                # Si es un único argumento, pasarlo tal cual
                 metodo_a_llamar(args)
         else:
-            print(f"{self} recibió un mensaje para {metodo}, pero no existe tal método.")
-
+            print(
+                f"{self} recibió un mensaje para {metodo}, pero no existe tal método."
+            )
 
     def ejemplo(self, dato, remitente):
-        print(f"[Ronda {self.env.now}] {self} ejecuta 'ejemplo' desde {remitente} con dato={dato}")
+        print(
+            f"[Ronda {self.env.now}] {self} ejecuta 'ejemplo' desde {remitente} con dato={dato}"
+        )
         destinatario = random.choice(list(self.vecinos))
-        destinatario.msg("ejemplo", (random.randint(0, 1000),), self)
+        numero_misterioso = random.randint(0, 1000)
+        print(
+            f"[Ronda {self.env.now}] {self} está enviando (ejemplo, {numero_misterioso}) a {destinatario}"
+        )
+        destinatario.msg("ejemplo", (numero_misterioso,), self)
+
 
 """
     Genera el número de nodos (que son objetos de la clase nodo) que se
@@ -101,6 +123,8 @@ class Nodo:
     dict:
         Diccionario de la forma {pid: nodo}
 """
+
+
 def construir_grafica(grado: int, env):
     # Generamos el número de nodos especificado
     nodos = {i: Nodo(i, env) for i in range(1, grado + 1)}
@@ -121,9 +145,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     env = simpy.Environment()
     grafica = construir_grafica(args.nodos, env)
-    print(f"Se ha generado una red de nodos de grado {args.nodos} con las siguientes adyacencias:")
+    print(
+        f"Se ha generado una red de nodos de grado {args.nodos} con las siguientes adyacencias:"
+    )
     for nodo in grafica:
         print(f"{grafica[nodo]}: {grafica[nodo].vecinos}")
 
-    grafica[2].msg("ejemplo", (39,), grafica[1])
+    grafica[2].start_dfs()
     env.run(until=10)
--- practica3/practica3.py
+++ practica3/practica3.py
@@ -2,14 +2,17 @@ import argparse
 import random
 
 parser = argparse.ArgumentParser(
-                    prog="practica3",
-                    description="Un programa para simular la red de un sistema distribuido y hallar el diámetro de su gráfica subyacente.",
-                    epilog="Implementado por Axel Rodríguez y José David Aguilar para el curso de Computación Distribuida 7106 2025-2.")
+    prog="practica3",
+    description="Un programa para simular la red de un sistema distribuido y hallar el diámetro de su gráfica subyacente.",
+    epilog="Implementado por Axel Rodríguez y José David Aguilar para el curso de Computación Distribuida 7106 2025-2.",
+)
 
-parser.add_argument("nodos",
-                    type=int,
-                    default=8,
-                    help="Número de nodos que tendrá el sistema distribuido")
+parser.add_argument(
+    "nodos",
+    type=int,
+    default=8,
+    help="Número de nodos que tendrá el sistema distribuido",
+)
 
 
 class nodo:
@@ -20,8 +23,8 @@ class nodo:
         self.dfs_parent = None
         self.bfs_children = set()
         self.bfs_parent = None
-        self.bfs_level = float('inf')
-        self.expected_msg = float('inf')
+        self.bfs_level = float("inf")
+        self.expected_msg = float("inf")
 
     def __repr__(self):
         return f"<{str(self.pid)}>"
@@ -47,14 +50,20 @@ class nodo:
         print(f"[RX] {self.dfs_parent} ahora es padre de {self}")
         print(f"[RX] Vecinos de {self}: {self.vecinos}, Visitados: {visited}")
         if self.vecinos.issubset(visited):
-            print(f"[RX] El conjunto de vecinos de {self} es subconjunto del de visitados.")
+            print(
+                f"[RX] El conjunto de vecinos de {self} es subconjunto del de visitados."
+            )
             visited.add(self)
-            print(f"[RX] Mandando BACK({visited}, {self}) y vaciando el conjunto de hijos.")
+            print(
+                f"[RX] Mandando BACK({visited}, {self}) y vaciando el conjunto de hijos."
+            )
             remitente.back_dfs(visited, self)
             self.dfs_children = set()
         else:
             dif = self.vecinos.difference(visited)
-            print(f"[RX] La diferencia de nodos entre visitados y vecinos de {self} es {dif}")
+            print(
+                f"[RX] La diferencia de nodos entre visitados y vecinos de {self} es {dif}"
+            )
             k = next(iter(dif))
             print(f"[RX] {k} ha sido selecionado como k de {self}")
             visited.add(self)
@@ -93,53 +102,81 @@ class nodo:
         if self.bfs_parent is None:
             print(f"[RX BFS] {self} no tiene padre, acepta la oferta de {remitente}")
             self.refresh_bfs(distancia, remitente)
-            print(f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}")
+            print(
+                f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}"
+            )
             if self.expected_msg == 0:
-                print(f"[RX BFS] {self} no tiene más vecinos a los que mandar mensajes, mandando BACK(Yes, {self.bfs_level}) al padre {self.bfs_parent}")
+                print(
+                    f"[RX BFS] {self} no tiene más vecinos a los que mandar mensajes, mandando BACK(Yes, {self.bfs_level}) al padre {self.bfs_parent}"
+                )
                 self.bfs_parent.back_bfs(True, distancia + 1, self)
             else:
                 destinatarios = self.vecinos.copy()
                 if remitente in destinatarios:
                     destinatarios.remove(remitente)
-                print(f"[RX BFS] {self} está enviando mensajes GO({self.bfs_level}) a sus vecinos: {destinatarios}")
+                print(
+                    f"[RX BFS] {self} está enviando mensajes GO({self.bfs_level}) a sus vecinos: {destinatarios}"
+                )
                 for k in destinatarios:
                     k.go_bfs(distancia + 1, self)
         elif self.bfs_level > distancia + 1:
-            print(f"[RX BFS] {self} ha encontrado que si es hijo de {remitente} su nivel mejorará de {self.bfs_level} a {distancia + 1}")
+            print(
+                f"[RX BFS] {self} ha encontrado que si es hijo de {remitente} su nivel mejorará de {self.bfs_level} a {distancia + 1}"
+            )
             self.refresh_bfs(distancia, remitente)
-            print(f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}")
+            print(
+                f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}"
+            )
             if self.expected_msg == 0:
-                print(f"[RX BFS] {self} no tiene más vecinos a los que mandar mensajes, mandando BACK(Yes, {self.bfs_level}) al padre {self.bfs_parent}")
+                print(
+                    f"[RX BFS] {self} no tiene más vecinos a los que mandar mensajes, mandando BACK(Yes, {self.bfs_level}) al padre {self.bfs_parent}"
+                )
                 self.bfs_parent.back_bfs(True, self.bfs_level, self)
             else:
                 destinatarios = self.vecinos.copy()
                 destinatarios.remove(remitente)
-                print(f"[RX BFS] {self} está enviando mensajes GO({self.bfs_level}) a sus vecinos: {destinatarios}")
+                print(
+                    f"[RX BFS] {self} está enviando mensajes GO({self.bfs_level}) a sus vecinos: {destinatarios}"
+                )
                 for k in destinatarios:
                     k.go_bfs(distancia + 1, self)
         else:
-            print(f"[RX BFS] {self} (nivel {self.bfs_level}) ha recibido una oferta de paternidad de {remitente} a {distancia + 1}, pero su nivel no mejoraría.")
+            print(
+                f"[RX BFS] {self} (nivel {self.bfs_level}) ha recibido una oferta de paternidad de {remitente} a {distancia + 1}, pero su nivel no mejoraría."
+            )
             print(f"[RX BFS] {self} mandando BACK(No, {self.bfs_level}) a {remitente}")
             remitente.back_bfs(False, distancia + 1, self)
 
     def back_bfs(self, cambiar_padre: bool, distancia: int, remitente):
-        print(f"[RX BFS] {self} recibió un mensaje BACK de {remitente} con estado ({cambiar_padre}, {distancia})")
-        print(f"[RX BFS] Distancia recibida de {remitente}: {distancia}. Nivel de {self}: {self.bfs_level}; ")
+        print(
+            f"[RX BFS] {self} recibió un mensaje BACK de {remitente} con estado ({cambiar_padre}, {distancia})"
+        )
+        print(
+            f"[RX BFS] Distancia recibida de {remitente}: {distancia}. Nivel de {self}: {self.bfs_level}; "
+        )
         print(f"[RX BFS] {distancia} == {self.bfs_level + 1}?")
         if distancia == self.bfs_level + 1:
             self.expected_msg -= 1
             if cambiar_padre is True:
                 self.bfs_children.add(remitente)
-                print(f"[RX BFS] {remitente} ha sido anexado en la lista de hijos de {self}")
+                print(
+                    f"[RX BFS] {remitente} ha sido anexado en la lista de hijos de {self}"
+                )
                 # self.expected_msg -= 1
-                print(f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}")
+                print(
+                    f"[RX BFS] {self}: Padre: {self.bfs_parent}, Hijos: {self.bfs_children}, Nivel: {self.bfs_level}, ExpMsg: {self.expected_msg}"
+                )
                 if self.expected_msg <= 0:
                     if self.bfs_parent is self:
                         print(f"[RX BFS] BFS Completado en {self}.")
                     else:
-                        print(f"[RX BFS] El nodo {self} ha terminado de computar. Mandando BACK(Yes, {self.bfs_level}) a su padre {self.bfs_parent}.")
+                        print(
+                            f"[RX BFS] El nodo {self} ha terminado de computar. Mandando BACK(Yes, {self.bfs_level}) a su padre {self.bfs_parent}."
+                        )
         else:
-            print(f"{self} está enterado de que {remitente} rechazó su oferta de paternidad")
+            print(
+                f"{self} está enterado de que {remitente} rechazó su oferta de paternidad"
+            )
 
         print(f"[RX BFS] {self} ahora espera {self.expected_msg} mensajes")
 
@@ -162,6 +199,8 @@ class nodo:
     dict:
         Diccionario de la forma {pid: nodo}
 """
+
+
 def construir_grafica(grado: int):
     # Generamos el número de nodos especificado
     nodos = {i: nodo(i) for i in range(1, grado + 1)}
@@ -184,6 +223,7 @@ def bfs(grafica):
         print(f"Mensajes esperados por {grafica[nodo]}: {grafica[nodo].expected_msg}")
         print(f"Hijos de {grafica[nodo]}: {grafica[nodo].bfs_children}")
 
+
 def dfs(grafica):
     grafica[1].start_dfs()
     for nodo in grafica:
@@ -193,7 +233,9 @@ def dfs(grafica):
 if __name__ == "__main__":
     args = parser.parse_args()
     grafica = construir_grafica(args.nodos)
-    print(f"Se ha generado una red de nodos de grado {args.nodos} con las siguientes adyacencias:")
+    print(
+        f"Se ha generado una red de nodos de grado {args.nodos} con las siguientes adyacencias:"
+    )
     for nodo in grafica:
         print(f"{grafica[nodo]}: {grafica[nodo].vecinos}")
 
